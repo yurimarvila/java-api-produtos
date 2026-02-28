c195875ccc5ed46b3a6a5c42fc30412e1984d35c
Yuri Marvila <yurimarvilapessoal@gmail.com>
2026-02-27 21:56:47 -0300

    Remove import desnecessário e ajusta formatação do mapeamento de requisições no ProdutoController

--- api-produtos/src/main/java/br/com/banco/api_produtos/controller/ProdutoController.java
+++ api-produtos/src/main/java/br/com/banco/api_produtos/controller/ProdutoController.java
@@ -2,8 +2,6 @@ package br.com.banco.api_produtos.controller;
 
 import java.util.ArrayList;
 
-import javax.swing.Spring;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.banco.api_produtos.dto.ProdutoDTO;
 import br.com.banco.api_produtos.model.Produto;
 import br.com.banco.api_produtos.service.ProdutoService;
 
 @RestController
-@RequestMapping ("/produtos")
+@RequestMapping("/produtos")
 
 public class ProdutoController {
     private ProdutoService service = new ProdutoService();
@@ -25,5 +24,25 @@ public class ProdutoController {
     @GetMapping
     public ArrayList<Produto> listarTodos() {
         return service.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Produto cadastrar(@RequestBody ProdutoDTO dto) {
+        return service.cadastrar(dto);
+    }
+
+    @PutMapping ("/{id}")
+    public Produto atualizar(@PathVariable Long id, @RequestBody ProdutoDTO dto) {
+        return service.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deletar(@PathVariable Long id) {
+        return service.deletar(id);
     }
 }
