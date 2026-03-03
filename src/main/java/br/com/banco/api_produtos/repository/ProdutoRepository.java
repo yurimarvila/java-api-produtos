7888bb28beabdb067057b4a0b2ac76bf3fa7064e
Yuri Marvila <yurimarvilapessoal@gmail.com>
2026-03-03 12:26:10 -0300

    Delete src directory

--- src/main/java/br/com/banco/api_produtos/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.banco.api_produtos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.banco.api_produtos.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-}
